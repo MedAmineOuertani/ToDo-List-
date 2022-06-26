5873d22f3cbb934726f4835afaf1130d081be784
amine <amine@amine.amine>
2022-06-26 08:24:39 -0400

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 const express=require("express");
 const bodyParser=require('body-parser');
+const date=require(__dirname+'/date.js');
 
 
 const app=express();
@@ -9,23 +10,29 @@ app.use(express.static('public'));
 app.set('view engine','ejs');
 
 let item=[];
+let work=[];
 
 app.get('/',function(req,res){
-    let today =new Date();
-    let options={
-        weekday:"long",
-        day:"numeric",
-        month : "long"
-    };
-    let day =today.toLocaleDateString("en-US",options);
-    res.render('list',{kindOfDay: day , newitem: item});
+    
+    res.render('list',{title: date( ) , newitem: item});
 });
 app.post('/',function(req,res){
-    item.push(String(req.body.item));
-    res.redirect('/');
+    if(req.body.button=='work'){
+        work.push(String(req.body.item));
+        res.redirect('/work');
+    }else{
+        item.push(String(req.body.item));
+        res.redirect('/');
+    }
+    
     
 });
-
+app.get('/work',function(req,res){
+    res.render('list',{title :"work",newitem: work});
+}); 
+app.get("/about",function(re,res){
+    res.render('about',{});
+});
 app.listen(process.env.PORT || 3000,function(){
     console.log("server is running on port 3000");
 });
---
+++ date.js
@@ -0,0 +1,11 @@
+module.exports = currentDay;
+function currentDay(){
+    let today =new Date();
+    let options={
+        weekday:"long",
+        day:"numeric",
+        month : "long"
+    };
+    let day =today.toLocaleDateString("en-US",options);
+    return day;
+}
